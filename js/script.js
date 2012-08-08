b2f73221b74b2be94f6c0aca4bfbba5a18856b4c
Byron Lutz <byronlutz@gmail.com>
2012-08-08 14:28:37 -0700

    Top portion is responsive down to tablet

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 /* Author:
-
+	Byron Lutz
 */
+
+$(document).ready(function() {
+	var width = $(window).width();
+	
+	if(width <= 979) {
+		$('.bighello-describe').parent().removeClass('span5').addClass('span8');
+		$('#latest-rotator').removeClass('span9').addClass('span12');
+		$('#latest-socialmedia-container').removeClass('span3').addClass('span12');
+	}
+});
 
 
 
