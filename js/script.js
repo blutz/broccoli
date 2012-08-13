37e2cfc9658660e456f8f86c901dd720a36f84fa
Byron Lutz <byronlutz@gmail.com>
2012-08-13 15:34:33 -0700

    Responsive design complete.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@
 $(document).ready(function() {
 	var width = $(window).width();
 	
+	// Portrait phone
+	if(width <= 480) {
+	}
+	
+	// Landscape phone
+	if(width <= 767) {
+		$('#access-projectlist').addClass("collapse access-collapse");
+	}
+	
 	if(width <= 979) {
 		$('.bighello-describe').parent().removeClass('span5').addClass('span8');
 		$('#latest-rotator').removeClass('span9').addClass('span12');
