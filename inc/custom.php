9a5fed012c081d76ebc630b74026527e5e891574
Byron Lutz <byronlutz@gmail.com>
2012-10-22 14:09:27 -0700

    Updated twitter URL

--- inc/custom.php
+++ inc/custom.php
@@ -1,1 +1,27 @@
 <?php 
+
+function cpoWidgets() {
+	// Register bighello sidebar
+	register_sidebar(array(
+		'name' => __( 'Top text' ),
+		'id' => 'top-text',
+		'description' => __( 'An area for the top information on the site.' ),
+		'before_widget' => '<span class="bighello-describe">',
+		'after_widget' => '</span>',
+		'before_title' => '',
+		'after_title' => ''		
+	));
+	
+	// Register access sidebar
+	register_sidebar(array(
+		'name' => __( 'Access (SIAC)' ),
+		'id' => 'siac',
+		'description' => __( 'Info about SIAC goes here' ),
+		'before_widget' => '<p>',
+		'after_widget' => '</p>',
+		'before_title' => '',
+		'after_title' => ''		
+	));
+}
+
+add_action('widgets_init', 'cpoWidgets');
--- inc/front-page.php
+++ inc/front-page.php
@@ -7,7 +7,7 @@ function latest_tweet() {
 
 // Private functions
 function get_latest_tweet() {
-	$twitter_url = 'https://twitter.com/statuses/user_timeline/uclacpo.xml?count=1';
+	$twitter_url = 'http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=uclacpo&count=1';
 	$xml = simplexml_load_file($twitter_url);
 	if(!isset($xml))
 		return "Failed to connect to twitter.";
