377d72fa1c4f7234ae2ee98bbab6ad7302852ae2
Byron Lutz <byronlutz@gmail.com>
2012-11-25 22:30:33 -0800

    Added footer menu integration. Fixes #2

--- footer.php
+++ footer.php
@@ -3,69 +3,40 @@
 		<div class="row">
 			<div class="span3 hidden-phone">
 				<h1>CPOSA <span>(Service)</span></h1>
-				<h2>Education caucus</h2>
-					<ul>
-						<a href="#"><li>4Real Hop									</li></a>
-						<a href="#"><li>Adelante Tutorial Project					</li></a>
-						<a href="#"><li>Advancing Careers in Engineering and Science</li></a>
-						<a href="#"><li>African Education Project					</li></a>
-						<a href="#"><li>Barrio Youth Alternatives Project			</li></a>
-						<a href="#"><li>Latina Guiding Latinas						</li></a>
-						<a href="#"><li>Projecting Minds							</li></a>
-						<a href="#"><li>SANAA										</li></a>
-					</ul>
-				<h2>Health caucus</h2>
-					<ul>
-						<a href="#"><li>Asian Pacific Health Corps	 </a></li>
-						<a href="#"><li>Black Hypertension Project	 </a></li>
-						<a href="#"><li>Black Latino Aids Project	 </a></li>
-						<a href="#"><li>Dental Education Outreach	 </a></li>
-						<a href="#"><li>Dot Org						 </a></li>
-						<a href="#"><li>Inglewood Clinic Group		 </a></li>
-						<a href="#"><li>Latino Student Health Project</a></li>
-						<a href="#"><li>UMMA Volunteer Program		 </a></li>
-					</ul>
+    			<?php wp_nav_menu(array(
+					'theme_location' => 'cposa_projects',
+					'container' => '',
+					'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+					'menu_class' => ''				
+				)); ?>
 			</div><!-- end div.span3 -->
 			<div class="span3 hidden-phone">
-				<h2>Social justice</h2>
-					<ul>
-						<a href="#"><li>Improving Dreams Equality, Access, and Success / AB540</li></a>
-						<a href="#"><li>IMHOME												  </li></a>
-						<a href="#"><li>Incarcerated Youth Tutorial Project					  </li></a>
-						<a href="#"><li>Project 1											  </li></a>
-						<a href="#"><li>Proyecto De Jornaleros								  </li></a>
-						<a href="#"><li>RAZA Youth Empowerment Project						  </li></a>
-						<a href="#"><li>Projecting Minds									  </li></a>
-					</ul>
 				<h1>SIAC <span>(Access)</span></h1>
-					<ul>
-						<a href="#"><li>American Indian Recruitment								   </li></a>
-						<a href="#"><li>Higher Opportunity Program for Education				   </li></a>
-						<a href="#"><li>MEChA Xinatchli											   </li></a>
-						<a href="#"><li>Mentors for Academic And Peer Support					   </li></a>
-						<a href="#"><li>Pacific Islander Education and Retention				   </li></a>
-						<a href="#"><li>Samahang Pilipino Advancing Community Empowerment		   </li></a>
-						<a href="#"><li>Students Heightening Academic Performance through Education</li></a>
-					</ul>
+    			<?php wp_nav_menu(array(
+					'theme_location' => 'siac_projects',
+					'container' => '',
+					'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+					'menu_class' => ''				
+				)); ?>
 			</div><!-- end div.span3 -->
 			<div class="span3 hidden-phone">
 				<h1>SRC <span>(Retention)</span></h1>
-				<ul>
-					<a href="#"><li>Academic supports program							   </li></a>
-					<a href="#"><li>MEChA Calmecac										   </li></a>
-					<a href="#"><li>Retention of American Indians Now!					   </li></a>
-					<a href="#"><li>Southeast Asian Campus Learning Education and Retention</li></a>
-					<a href="#"><li>Samahang Pilipino Education and Retention			   </li></a>
-					<a href="#"><li>Writing Success Program								   </li></a>
-				</ul>
+    			<?php wp_nav_menu(array(
+					'theme_location' => 'src_projects',
+					'container' => '',
+					'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+					'menu_class' => ''				
+				)); ?>
 				<h1>CPO</h1>
-				<ul>
-					<a href="#"><li>Contact us			 </li></a>
-					<a href="#"><li>FAQ					 </li></a>
-					<a href="#"><li>Apply to be an intern</li></a>
-					<li class="seperator"></li>
-					<a href="#"><li>Facebook</li></a>
-					<a href="#"><li>Twitter</li></a>
+    			<?php wp_nav_menu(array(
+					'theme_location' => 'cposa_nav',
+					'container' => '',
+					'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+					'menu_class' => ''				
+				)); ?>
+				<ul class="footer-sm">
+					<a href="https://www.facebook.com/uclacpo"><li>Facebook</li></a>
+					<a href="http://twitter.com/uclacpo"><li>Twitter</li></a>
 				</ul>
 			</div><!-- end div.span3 -->
 			<div class="span3" id="footer-contact">
