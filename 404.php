3b30192865b64768c8974c19b1cfe148b21c464e
Byron Lutz <byronlutz@gmail.com>
2014-01-22 20:02:34 -0800

    Made some changes to the 404 page

--- 404.php
+++ 404.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
   <?php roots_content_before(); ?>
+  <div class="container">
     <div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
     <?php roots_main_before(); ?>
       <div id="main" class="<?php echo FULLWIDTH_CLASSES; ?>" role="main">
         <div class="page-header">
-          <h1><?php _e('File Not Found', 'roots'); ?></h1>
+          <h1><?php _e('Page not found', 'roots'); ?></h1>
         </div>
         <div class="alert alert-block fade in">
-          <a class="close" data-dismiss="alert">&times;</a>
           <p><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'roots'); ?></p>
         </div>
         <p><?php _e('Please try the following:', 'roots'); ?></p>
@@ -20,5 +20,6 @@
       </div><!-- /#main -->
     <?php roots_main_after(); ?>
     </div><!-- /#content -->
+  </div><!-- .container -->
   <?php roots_content_after(); ?>
 <?php get_footer(); ?>
